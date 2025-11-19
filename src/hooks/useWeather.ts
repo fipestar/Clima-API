0702906775416d4f84d753a50f5e280d47cafd79
Juan David <barbosaneira@gmail.com>
2025-11-19 14:15:24 -0500

    type assignament

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,15 +1,24 @@
 import axios from "axios";
-import type { SearchType } from "../types";
+import { type Weather, type SearchType } from "../types";
 
 export default function useWeather() {
     const fetchWeather = async (search: SearchType) => {
-        const appid = "0f283923b5a734ca68ca96ed99217a45"
+        const appid = import.meta.env.VITE_API_KEY;
         
         try {
             const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appid}`
             
             const {data} = await axios(geoUrl);
-            console.log(data);
+            
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+
+            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appid}`
+           
+            const {data: weatherResult} = await axios<Weather>(weatherUrl);
+            console.log(weatherResult.main.temp);
+            console.log(weatherResult.main.temp_max);
+            console.log(weatherResult.name)
         } catch (error) {
             console.log(error);
         }
